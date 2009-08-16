74d4cf40ce41a71ec6fba0ef48de7f7a89a7f549
Jos van den Oever <oever@lapjos.(none)>
2009-08-16 13:48:46 +0200

    Implement parsing of 4bit and 12bit fields better.

--- src/mso/javaparser/LEInputStream.java
+++ src/mso/javaparser/LEInputStream.java
@@ -17,6 +17,8 @@ public class LEInputStream {
 
 	final LinkedList<ResettableInputStream> marks = new LinkedList<ResettableInputStream>();
 
+	int uint4leftover;
+
 	class ResettableInputStream extends InputStream {
 		final DataInputStream input;
 		int pos = 0;
@@ -66,7 +68,7 @@ public class LEInputStream {
 	public void rewind(Object mark) throws IOException {
 		ResettableInputStream m = marks.getLast();
 		if (m != mark) {
-			System.out.println("marks size:" +marks.size());
+			System.out.println("marks size:" + marks.size());
 			throw new Error("Logic error: mark was not set. " + m + " " + mark);
 		}
 		m.resetStream();
@@ -81,7 +83,10 @@ public class LEInputStream {
 	}
 
 	public byte readuint4() throws IOException {
-		return 0;
+		// TODO: we only support reading uint4 then uint12, make this general if
+		// needed
+		uint4leftover = Short.reverseBytes(input.readShort());
+		return (byte) (uint4leftover & 0xF);
 	}
 
 	public byte readuint8() throws IOException {
@@ -89,7 +94,7 @@ public class LEInputStream {
 	}
 
 	public short readuint12() throws IOException {
-		short s = Short.reverseBytes(input.readShort());
+		short s = (short) uint4leftover;
 		s = (short) (s >> 4);
 		return s;
 	}
