313b78ef5d5d00ec3a3f13222d88487f8339cc71
Jos van den Oever <oever@lapjos.(none)>
2009-09-12 17:49:07 +0200

    Implement the read functions.

--- cpp/leinputstream.h
+++ cpp/leinputstream.h
@@ -1,51 +1,216 @@
 #ifndef LEINPUTSTREAM_H
 #define LEINPUTSTREAM_H
 
-#include <QtCore/QtGlobal>
+#include <QtCore/QIODevice>
+#include <QtCore/QDataStream>
 #include <exception>
 
-class IncorrectValueException : public std::exception {
+class IOException : public std::exception {
 public:
-    IncorrectValueException(const QString&) {}
+    const QString msg;
+    IOException(const QString& m) :msg(m) {}
+    ~IOException() throw() {}
 };
 
-class EOFException : public std::exception {
+class IncorrectValueException : public IOException {
 public:
-    EOFException(const QString&) {}
+    IncorrectValueException(const QString& msg) :IOException(msg) {}
+    ~IncorrectValueException() throw() {}
+};
+
+class EOFException : public IOException {
+public:
+    EOFException(const QString& msg) :IOException(msg) {}
+    ~EOFException() throw() {}
 };
 
 class LEInputStream {
+private:
+    QIODevice* input;
+    QDataStream data;
+
+    quint8 bitfieldpos;
+    quint8 bitfield;
+
+    quint8 getBits(quint8 n) {
+        if (bitfieldpos < 0) {
+            data >> bitfield;
+            if (data.status() != QDataStream::Ok) {
+                throw IOException("Cannot read byte.");
+            }
+            bitfieldpos = 0;
+        }
+        quint8 v = bitfield >> bitfieldpos;
+        bitfieldpos += n;
+        if (bitfieldpos == 8) {
+            bitfieldpos = -1;
+        } else if (bitfieldpos > 8) {
+            throw IOException("Bitfield does not have enough bits left.");
+        }
+        return v;
+    }
+    void checkForLeftOverBits() {
+        if (bitfieldpos >= 0) {
+            throw IOException("Cannot read this type halfway through a bit operation.");
+        }
+    }
+
 public:
     class Mark {
+    friend class LEInputStream;
+    private:
+        QIODevice* input;
+        qint64 pos;
+        Mark(QIODevice* in) :input(in), pos((in) ?in->pos() :0) {} 
     public:
-         ~Mark() {
-             // TODO: release mark from stream
-         }
+        Mark() :input(0), pos(0) {} 
     };
 
-    Mark setMark() { return Mark(); }
-    void rewind(const Mark& m) {}
+    LEInputStream(QIODevice* in) :input(in), data(in) {
+        data.setByteOrder(QDataStream::LittleEndian);
+    }
 
-    bool readbit() { return false; }
-    quint8 readuint2() { return 0; }
-    quint8 readuint3() { return 0; }
-    quint8 readuint4() { return 0; }
-    quint8 readuint5() { return 0; }
-    quint8 readuint6() { return 0; }
-    quint8 readuint7() { return 0; }
-    quint8 readuint8() { return 0; }
-    quint8 readuint9() { return 0; }
-    quint16 readuint12() { return 0; }
-    quint16 readuint14() { return 0; }
-    quint16 readuint15() { return 0; }
-    quint16 readuint16() { return 0; }
-    quint16 readuint20() { return 0; }
-    quint32 readuint30() { return 0; }
-    quint32 readuint32() { return 0; }
-    qint16 readint16() { return 0; }
-    qint32 readint32() { return 0; }
+    Mark setMark() { return Mark(input); }
+    void rewind(const Mark& m) {
+        if (!m.input || !m.input->seek(m.pos)) {
+            throw IOException("Cannot rewind.");
+        }
+    }
 
-    quint32 getPosition() const { return 0; }
+    bool readbit() {
+        quint8 v = getBits(1) & 1;
+        return v == 1;
+    }
+
+    quint8 readuint2() {
+        return getBits(2) & 3;
+    }
+
+    quint8 readuint3() {
+        return getBits(3) & 0x7;
+    }
+
+    quint8 readuint4() {
+        return getBits(4) & 0xF;
+    }
+
+    quint8 readuint5() {
+        return getBits(5) & 0x1F;
+    }
+
+    quint8 readuint6() {
+        return getBits(6) & 0x3F;
+    }
+
+    quint8 readuint7() {
+        return getBits(7) & 0x7F;
+    }
+
+    quint16 readuint9() {
+        quint8 a = readuint8();
+        quint8 b = getBits(1) & 0x1;
+        quint8 v = (b << 8) | a;
+        return (short) v;
+    }
+
+    quint16 readuint12() {
+        // we assume there are 4 bits left
+        quint8 a = getBits(4) & 0xF;
+        quint8 b = readuint8();
+        return (b << 4) | a;
+    }
+
+    quint16 readuint14() {
+        quint16 v;
+        if (bitfieldpos < 0) {
+            quint8 a = readuint8();
+            quint8 b = getBits(6) & 0x3F;
+            v = (b << 8) | a;
+        } else if (bitfieldpos == 2) {
+            quint8 a = getBits(6) & 0x3F;
+            quint8 b = readuint8();
+            v = (b << 6) | a;
+        } else {
+            throw IOException("Cannot read this type halfway through a bit operation.");
+        }
+        return v;
+    }
+
+    quint16 readuint15() {
+        // we assume there are 7 bits left
+        quint8 a = getBits(7) & 0x7F;
+        quint8 b = readuint8();
+        return (b << 7) | a;
+    }
+
+    quint32 readuint20() {
+        quint8 v;
+        if (bitfieldpos < 0) {
+            quint8 a = readuint8();
+            quint8 b = readuint8();
+            quint8 c = getBits(4) & 0xF;
+            v = (c << 16) | (b << 8) | a;
+        } else if (bitfieldpos == 4) {
+            quint8 a = getBits(4) & 0xF;
+            quint8 b = readuint8();
+            quint8 c = readuint8();
+            v = (c << 12) | (b << 4) | a;
+        } else {
+            throw IOException("Cannot read this type halfway through a bit operation.");
+        }
+        return v;
+    }
+
+    quint32 readuint30() {
+        checkForLeftOverBits();
+        quint8 a = readuint8();
+        quint8 b = readuint8();
+        quint8 c = readuint8();
+        quint8 d = getBits(6) & 0x3F;
+        return (d << 24) | (c << 16) | (b << 8) | a;
+    }
+
+    quint8 readuint8() {
+        checkForLeftOverBits();
+        quint8 a;
+        data >> a;
+        if (data.status() != QDataStream::Ok) { throw IOException("Error reading data."); }
+        return a;
+    }
+
+    qint16 readint16() {
+        checkForLeftOverBits();
+        qint16 v;
+        data >> v;
+        if (data.status() != QDataStream::Ok) { throw IOException("Error reading data."); }
+        return v;
+    }
+
+    quint16 readuint16() {
+        checkForLeftOverBits();
+        quint16 v;
+        data >> v;
+        if (data.status() != QDataStream::Ok) { throw IOException("Error reading data."); }
+        return v;
+    }
+
+    quint32 readuint32() {
+        checkForLeftOverBits();
+        quint32 v;
+        data >> v;
+        if (data.status() != QDataStream::Ok) { throw IOException("Error reading data."); }
+        return v;
+    }
+
+    qint32 readint32() {
+        checkForLeftOverBits();
+        qint32 v;
+        data >> v;
+        if (data.status() != QDataStream::Ok) { throw IOException("Error reading data."); }
+        return v;
+    }
+
+    quint64 getPosition() const { return input->pos(); }
 };
 
 #endif
