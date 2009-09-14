ae7ff6a0ed7e04abdeebec493d5b3b3e2cea363e
Jos van den Oever <oever@lapjos.(none)>
2009-09-15 00:10:37 +0200

    Make LEInputStream based on a bytearray to fix a hard bug with rewinding the stream. It is a shame this is needed and maybe it is possible to go back to a real stream at some point.

--- src/mso/javaparser/LEInputStream.java
+++ src/mso/javaparser/LEInputStream.java
@@ -1,87 +1,14 @@
 package mso.javaparser;
 
-import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.LinkedList;
 
 /**
  * byte = int8 short = int16 char = uint16 int = int32 long = int64
  */
 public class LEInputStream {
 
-	class RewindableInputStream extends InputStream {
-
-		int[] buffer = new int[64];
-
-		int pos;
-
-		int bytesInBuffer;
-
-		final LinkedList<Integer> marks = new LinkedList<Integer>();
-
-		final InputStream input;
-
-		RewindableInputStream(InputStream in) {
-			input = in;
-			pos = -1;
-		}
-
-		@Override
-		public int read() throws IOException {
-			if (pos < 0)
-				// no mark is set and no data in the buffer
-				return input.read();
-
-			if (pos == bytesInBuffer) {
-				if (marks.size() == 0) {
-					// buffer is depleted, going back to passing data directly
-					pos = -1;
-					return input.read();
-				}
-				if (pos == buffer.length) {
-					buffer = Arrays.copyOf(buffer, 2 * buffer.length);
-				}
-				buffer[pos] = input.read();
-				bytesInBuffer++;
-			}
-
-			return buffer[pos++];
-		}
-
-		public Integer setMark() {
-			Integer mark;
-			if (marks.size() == 0) {
-				mark = pos = bytesInBuffer = 0;
-			} else {
-				mark = pos;
-			}
-			marks.add(mark);
-			return mark;
-		}
-
-		public void releaseMark(Object mark) {
-			Integer m = marks.removeLast();
-			if (m != mark) {
-				throw new Error("Logic error: mark was not set.");
-			}
-		}
-
-		public void rewind(Object mark) throws IOException {
-			Integer m = marks.getLast();
-			if (m != mark) {
-				System.out.println("marks size:" + marks.size());
-				throw new Error("Logic error: mark was not set. " + m + " "
-						+ mark);
-			}
-			pos = m;
-		}
-
-	}
-
-	final RewindableInputStream rewindableInputStream;
-	final DataInputStream input;
+	final byte data[];
 	int pos;
 
 	private class Mark {
@@ -95,41 +22,36 @@ public class LEInputStream {
 	int bitfield;
 	int bitfieldpos;
 
-	public LEInputStream(InputStream i) {
-		rewindableInputStream = new RewindableInputStream(i);
-		input = new DataInputStream(rewindableInputStream);
+	public LEInputStream(byte d[]) {
+		data = d;
 		bitfieldpos = -1;
 		pos = 0;
 	}
 
 	public Mark setMark() {
-		return new Mark(rewindableInputStream.setMark());
+		return new Mark(pos);
 	}
 
 	public void releaseMark(Object mark) {
-		Mark m = (Mark) mark;
-		rewindableInputStream.releaseMark(m.pos);
 	}
 
 	public int distanceFromMark(Object mark) {
 		Mark m = (Mark) mark;
-		return rewindableInputStream.pos - m.pos;
+		return pos - m.pos;
 	}
 
-	public void rewind(Object m) throws IOException {
+	public void rewind(Object m) {
 		Mark mark = (Mark) m;
-		pos -= rewindableInputStream.pos - mark.pos;
-		rewindableInputStream.rewind(mark.pos);
+		pos = mark.pos;
 	}
 
-	public boolean atEnd() throws IOException {
-		return input.available() == 0;
+	public boolean atEnd() {
+		return pos == data.length;
 	}
 
 	private int getBits(int n) throws IOException {
 		if (bitfieldpos < 0) {
-			bitfield = input.readByte();
-			pos++;
+			bitfield = readUnsignedByte();
 			bitfieldpos = 0;
 		}
 		int v = bitfield >> bitfieldpos;
@@ -142,10 +64,16 @@ public class LEInputStream {
 		return v;
 	}
 
-	private void checkForLeftOverBits() throws IOException {
+	private int readUnsignedByte() throws IOException {
 		if (bitfieldpos >= 0)
 			throw new IOException(
 					"Cannot read this type halfway through a bit operation.");
+		if (pos >= data.length)
+			throw new EOFException();
+		int v = data[pos++];
+		if (v < 0)
+			v = 0x100 + v;
+		return v;
 	}
 
 	public boolean readbit() throws IOException {
@@ -184,9 +112,8 @@ public class LEInputStream {
 	}
 
 	public short readuint9() throws IOException {
-		int a = input.readByte();
+		int a = readUnsignedByte();
 		int b = getBits(1) & 0x1;
-		pos++;
 		int v = (b << 8) | a;
 		return (short) v;
 	}
@@ -194,8 +121,7 @@ public class LEInputStream {
 	public short readuint12() throws IOException {
 		// we assume there are 4 bits left
 		int a = getBits(4) & 0xF;
-		int b = input.readByte();
-		pos++;
+		int b = readUnsignedByte();
 		int v = (b << 4) | a;
 		return (short) v;
 	}
@@ -203,14 +129,12 @@ public class LEInputStream {
 	public short readuint14() throws IOException {
 		int v;
 		if (bitfieldpos < 0) {
-			int a = input.readByte();
-			pos++;
+			int a = readUnsignedByte();
 			int b = getBits(6) & 0x3F;
 			v = (b << 8) | a;
 		} else if (bitfieldpos == 2) {
 			int a = getBits(6) & 0x3F;
-			int b = input.readByte();
-			pos++;
+			int b = readUnsignedByte();
 			v = (b << 6) | a;
 		} else {
 			throw new IOException(
@@ -222,8 +146,7 @@ public class LEInputStream {
 	public short readuint15() throws IOException {
 		// we assume there are 7 bits left
 		int a = getBits(7) & 0x7F;
-		int b = input.readByte();
-		pos++;
+		int b = readUnsignedByte();
 		int v = (b << 7) | a;
 		return (short) v;
 	}
@@ -231,16 +154,14 @@ public class LEInputStream {
 	public int readuint20() throws IOException {
 		int v;
 		if (bitfieldpos < 0) {
-			int a = input.readByte();
-			int b = input.readByte();
-			pos += 2;
+			int a = readUnsignedByte();
+			int b = readUnsignedByte();
 			int c = getBits(4) & 0xF;
 			v = (c << 16) | (b << 8) | a;
 		} else if (bitfieldpos == 4) {
 			int a = getBits(4) & 0xF;
-			int b = input.readByte();
-			int c = input.readByte();
-			pos += 2;
+			int b = readUnsignedByte();
+			int c = readUnsignedByte();
 			v = (c << 12) | (b << 4) | a;
 		} else {
 			throw new IOException(
@@ -250,50 +171,55 @@ public class LEInputStream {
 	}
 
 	public int readuint30() throws IOException {
-		checkForLeftOverBits();
-		int a = input.readByte();
-		int b = input.readByte();
-		int c = input.readByte();
-		pos += 3;
+		int a = readUnsignedByte();
+		int b = readUnsignedByte();
+		int c = readUnsignedByte();
 		int d = getBits(6) & 0x3F;
 		int v = (d << 24) | (c << 16) | (b << 8) | a;
 		return v;
 	}
 
 	public byte readuint8() throws IOException {
-		checkForLeftOverBits();
-		pos++;
-		return input.readByte();
+		return (byte) readUnsignedByte();
 	}
 
 	public short readint16() throws IOException {
-		checkForLeftOverBits();
-		pos += 2;
-		return Short.reverseBytes(input.readShort());
+		int a = readUnsignedByte();
+		int b = readUnsignedByte();
+		int v = (b << 8) | a;
+		return (short) v;
 	}
 
 	public int readuint16() throws IOException {
-		checkForLeftOverBits();
-		pos += 2;
-		int s = Short.reverseBytes(input.readShort());
-		if (s < 0)
-			s = 0x10000 + s;
-		return s;
+		int a = readUnsignedByte();
+		int b = readUnsignedByte();
+		int v = (b << 8) | a;
+		return v;
 	}
 
 	public int readuint32() throws IOException {
-		checkForLeftOverBits();
-		pos += 4;
-		return Integer.reverseBytes(input.readInt());
+		int a = readUnsignedByte();
+		int b = readUnsignedByte();
+		int c = readUnsignedByte();
+		int d = readUnsignedByte();
+		int v = (d << 24) | (c << 16) | (b << 8) | a;
+		return v;
 	}
 
 	public int readint32() throws IOException {
-		checkForLeftOverBits();
-		pos += 4;
-		return Integer.reverseBytes(input.readInt());
+		int a = readUnsignedByte();
+		int b = readUnsignedByte();
+		int c = readUnsignedByte();
+		int d = readUnsignedByte();
+		int v = (d << 24) | (c << 16) | (b << 8) | a;
+		return v;
 	}
 
 	public int getPosition() {
 		return pos;
+	}
+
+	public int getSize() {
+		return data.length;
 	}
 }
